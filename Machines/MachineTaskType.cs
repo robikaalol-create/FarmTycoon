1d1e12e59474de321ac3cc7f4dcbe4f8059746b8
robikaalol-create <robikaalol@gmail.com>
2026-08-16 15:22:17 +0200

    Create MachineTaskType.cs

---
+++ Machines/MachineTaskType.cs
@@ -0,0 +1,19 @@
+namespace FarmTycoon.Machines
+{
+    public enum MachineTaskType
+    {
+        Idle,
+        Plowing,
+        Cultivating,
+        Seeding,
+        Spraying,
+        Fertilizing,
+        Harvesting,
+        Transport,
+        Loading,
+        Unloading,
+        Maintenance,
+        Refueling,
+        Moving
+    }
+}
