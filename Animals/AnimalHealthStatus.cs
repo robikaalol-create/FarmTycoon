b389c74129e23341ff085db12ca7092b8ca887df
robikaalol-create <robikaalol@gmail.com>
2026-08-16 15:12:57 +0200

    Create AnimalHealthStatus.cs

---
+++ Animals/AnimalHealthStatus.cs
@@ -0,0 +1,12 @@
+namespace FarmTycoon.Animals
+{
+    public enum AnimalHealthStatus
+    {
+        Healthy,
+        Stressed,
+        Sick,
+        Injured,
+        Quarantined,
+        Critical
+    }
+}
