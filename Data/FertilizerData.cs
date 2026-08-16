3beddd8d5481ca4a9cb2d094b0a02571d9cb188a
robikaalol-create <robikaalol@gmail.com>
2026-08-16 14:52:10 +0200

    Add FertilizerData class for fertilizer properties

---
+++ Data/FertilizerData.cs
@@ -0,0 +1,13 @@
+namespace FarmTycoon.Data
+{
+    public class FertilizerData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public float NitrogenBoost { get; set; }
+        public float PhosphorusBoost { get; set; }
+        public float PotassiumBoost { get; set; }
+        public float Cost { get; set; }
+        public float NegativeEffect { get; set; }
+    }
+}
