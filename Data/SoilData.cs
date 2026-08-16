e946763e5a12079f88234b3a7493b3b506bad4d3
robikaalol-create <robikaalol@gmail.com>
2026-08-16 14:51:15 +0200

    Add SoilData class with soil properties

---
+++ Data/SoilData.cs
@@ -0,0 +1,14 @@
+namespace FarmTycoon.Data
+{
+    public class SoilData
+    {
+        public float Fertility { get; set; } = 0.5f;
+        public float Moisture { get; set; } = 0.5f;
+        public float Nitrogen { get; set; } = 0.5f;
+        public float Phosphorus { get; set; } = 0.5f;
+        public float Potassium { get; set; } = 0.5f;
+        public float PH { get; set; } = 7.0f;
+        public float Compaction { get; set; } = 0.0f;
+        public float WeedLevel { get; set; } = 0.0f;
+    }
+}
