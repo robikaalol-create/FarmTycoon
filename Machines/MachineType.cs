a66570893dd4edbfc60727eb6419ddc39396598d
robikaalol-create <robikaalol@gmail.com>
2026-08-16 15:21:47 +0200

    Create MachineType.cs

---
+++ Machines/MachineType.cs
@@ -0,0 +1,25 @@
+namespace FarmTycoon.Machines
+{
+    public enum MachineType
+    {
+        Tractor,
+        Harvester,
+        Seeder,
+        Cultivator,
+        Plow,
+        Sprayer,
+        FertilizerSpreader,
+        Mower,
+        Baler,
+        Trailer,
+        LightTruck,
+        HeavyTruck,
+        RefrigeratedTruck,
+        TankerTruck,
+        Loader,
+        Forklift,
+        Crane,
+        ForestryMachine,
+        SnowPlow
+    }
+}
