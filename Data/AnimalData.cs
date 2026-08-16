9adbcbec63d50600ea7f8a6f6843423bfa5df126
robikaalol-create <robikaalol@gmail.com>
2026-08-16 14:49:26 +0200

    Add AnimalData class for farm management

---
+++ Data/AnimalData.cs
@@ -0,0 +1,14 @@
+namespace FarmTycoon.Data
+{
+    public class AnimalData
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Product { get; set; }
+        public string FeedType { get; set; }
+        public float FeedAmountPerDay { get; set; }
+        public float ProductPerDay { get; set; }
+        public float BasePrice { get; set; }
+        public float PurchaseCost { get; set; }
+    }
+}
