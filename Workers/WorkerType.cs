a5eedba6e38a7defa7598d3443f55ec9980676e5
robikaalol-create <robikaalol@gmail.com>
2026-08-16 15:28:41 +0200

    Create WorkerType.cs

---
+++ Workers/WorkerType.cs
@@ -0,0 +1,16 @@
+namespace FarmTycoon.Workers
+{
+    public enum WorkerType
+    {
+        FarmWorker,
+        AnimalCaretaker,
+        MachineOperator,
+        TruckDriver,
+        Mechanic,
+        Agronomist,
+        Veterinarian,
+        LogisticsManager,
+        FarmManager,
+        AISpecialist
+    }
+}
